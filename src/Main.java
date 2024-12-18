8bca1c4788faf5540ea575e287ade29cb0c64035
614925 <senter.614925@swl.k12.oh.us>
2024-12-18 08:13:32 -0500

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        //Testing methods from InputHelper
+        Scanner scan = new Scanner(System.in);
+
+        //Testing getInt
+        double x = InputHelper.getRangedDouble(scan, "please enter in an double value between 0.0-100.0", 0.0, 100.0);
+        System.out.println("My number is " + x);
+
+    }
+        //Testing getString
 }
